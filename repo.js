6a7de894c47d53da8188ca6b88e648d713807214
WilliamDavi <82419384+WilliamDavi@users.noreply.github.com>
2021-04-16 19:09:34 -0300

    Add files via upload

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "´3´ william `3`#4659",
+	"id": "WilliamDavi",
+	"patches": {
+		"DoubleMix": "A th13 patch then can have the Original music with the Spirit world version (tip: this patch is a test)"
+	},
+	"servers": [
+		"https://github.com/WilliamDavi/WilliamDavi/"
+	],
+	"title": "This Repository maybe have some bgm and bullets edits"
+}
