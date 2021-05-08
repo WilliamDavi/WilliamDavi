ab88ad2c82986a83ab1d4fa80084951ff5fe29ce
WilliamDavi <82419384+WilliamDavi@users.noreply.github.com>
2021-05-08 15:22:15 -0300

    Add files via upload

---
+++ Th18LilyWhiteSpriteFix/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "Th18LilyWhiteSpriteFix",
+	"servers": [
+		"https://mirrors.thpatch.net/WilliamDavi/Th18LilyWhiteSpriteFix/"
+	],
+	"title": "Lily White card sprite with the th16 Lily White sprite."
+}
--- repo.js
+++ repo.js
@@ -3,7 +3,7 @@
 	"id": "WilliamDavi",
 	"patches": {
 		"DoubleMix": "A th13 patch then can have the Original music with the Spirit world version (tip: this patch is a test)",
-		"Th18-LilyWhiteSpriteFix": "Lily White card sprite with the th16 Lily White sprite"
+		"Th18LilyWhiteSpriteFix": "Lily White card sprite with the th16 Lily White sprite"
 	},
 	"servers": [
 		"https://mirrors.thpatch.net/WilliamDavi/"
