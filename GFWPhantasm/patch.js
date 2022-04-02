f149b2f1c7c389fad0daee2ffd09284f56cda066
WilliamDavi <82419384+WilliamDavi@users.noreply.github.com>
2022-04-02 19:43:31 -0300

    Add files via upload

---
+++ GFWPhantasm/patch.js
@@ -0,0 +1,13 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa",
+		"nmlgc/script_latin",
+		"nmlgc/western_name_order",
+		"thpatch/lang_en"
+	],
+	"id": "GFWPhantasm",
+	"servers": [
+		"https://mirrors.thpatch.net/WilliamDavi/GFWPhantasm/"
+	],
+	"title": "An Phantasm Stage for GLW"
+}
