8740f2ec7780a0dcb058d120bc349b5b3758827e
WilliamDavi <82419384+WilliamDavi@users.noreply.github.com>
2023-08-26 18:43:53 -0300

    Update patch.js

--- DoubleMix/patch.js
+++ DoubleMix/patch.js
@@ -6,5 +6,5 @@
 	"servers": [
 		"https://mirrors.thpatch.net/WilliamDavi/DoubleMix/"
 	],
-	"title": "A th13 patch then can have the Original music with the Spirit world version (tip: this patch is a test)."
+	"title": "A th13 patch then can have the Original music with the Spirit world version (PS: this patch is a test)."
 }
